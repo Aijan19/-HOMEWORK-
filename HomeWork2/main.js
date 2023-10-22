a149281c91dc325ab0d9d70a30158d832084b2a5
Aijan19 <kelgenbaevaajzana@gmail.com>
2023-10-23 00:09:24 +0600

    Home Work 2

---
+++ HomeWork2/main.js
@@ -0,0 +1,22 @@
+// ? Задача - 1
+
+const Name = 'Айжан' 
+const Age = '21'
+const hometown = 'Каракол'
+
+console.log(Name, Age, hometown)
+
+//? Задача - 2
+
+const fav_num = 73
+const fav_col = 'black'
+
+console.log(typeof fav_col)
+console.log(typeof fav_num)
+
+// ? Задача - 3
+
+const firWord = 'Hello '
+const secWord = 'world!'
+
+console.log(firWord + secWord)
